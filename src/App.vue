20b331dfd573cb00e6d7a91dc6a86fefc6a22e43
samuelfilipefaria <samuelfilipefaria@gmail.com>
2023-12-01 18:35:14 -0300

    Configuring apollo graphql to get the API data

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,38 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import gql from 'graphql-tag'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      characters: {}
+    }
+  },
   components: {
     HelloWorld
+  },
+  apollo: {
+    characters: gql`query {
+      characters(page: 2, filter: { name: "rick" }) {
+        info {
+          count
+        }
+        results {
+          name
+        }
+      }
+      location(id: 1) {
+        id
+      }
+      episodesByIds(ids: [1, 2]) {
+        id
+      }
+    }`,
+  },
+  mounted() {
+    console.log(this.characters);
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://rickandmortyapi.com/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+const app = createApp(App)
+app.use(apolloProvider)
+app.mount('#app')
